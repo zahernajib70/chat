4b084a8e1a935e2af12002f394d7873c5140e8a6
Zaher Najib <zahernajib78@gmail.com>
2022-06-06 15:50:58 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// app.get('/z', (req, res) => {
+//     res.sendFile(__dirname + '/zouzou.js');
+//   });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
